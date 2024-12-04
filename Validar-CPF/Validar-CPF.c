11a2e1354897b5af343dd6343f4ac5124846be78
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2024-12-04 00:41:45 -0400

    Validar CPF
    Código que valida CPF, informando se o CPF é verdadeiro ou não.

---
+++ Validar-CPF/Validar-CPF.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+typedef struct{
+    int dgt[9];
+    int DVs[2];
+}CPF;
+
+CPF* ler_CPF();
+int DV1(CPF*);
+int DV2(CPF*);
+
+int main(){
+    CPF *ptr;
+    ptr = ler_CPF();
+
+    if(ptr->DVs[0] == DV1(ptr) && ptr->DVs[1] == DV2(ptr)){
+        printf("\nCPF valido.\n");
+    }
+    else{
+        printf("\nCPF invalido.\n");
+    }
+
+    return 0;
+}
+
+CPF* ler_CPF(){
+    CPF *ptr = malloc(sizeof(CPF));
+    int i;
+
+    printf("Informe seu CPF: ");
+    for(i=0; i<9; i++){
+        //scanf("%1d", &ptr->dgt[i]);
+        ptr->dgt[i] = getche() - '0';
+        if(i == 2 || i == 5){
+            printf(".");
+        }
+    }
+    for(i=0; i<2; i++){
+        //scanf("%1d", &ptr->DVs[i]);
+        if(i == 0){
+            printf("-");
+        }
+        ptr->DVs[i] = getche() - '0';
+    }
+    return ptr;
+}
+
+int DV1(CPF *ptr){
+    int i, j, soma = 0;
+    CPF *temp = malloc(sizeof(CPF));
+
+    for(i = 10, j=0; i>1 && j<9; i--, j++){
+        temp->dgt[j] = ptr->dgt[j] * i;
+        soma = soma + temp->dgt[j];
+    }
+    free(temp);
+    return 11 - soma % 11;
+}
+
+int DV2(CPF *ptr){
+    int i, j, soma = 0;
+    CPF *temp = malloc(sizeof(CPF));
+    for(i = 11, j=0; i>2 && j<9; i--, j++){
+        temp->dgt[j] = ptr->dgt[j] * i;
+        soma = soma + temp->dgt[j];
+    }
+    soma = soma + DV1(ptr) * 2;
+    free(temp);
+    return 11 - soma % 11;
+}
