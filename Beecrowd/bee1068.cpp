9408b6d48cab6d2ad827698c036737719167cb60
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2025-04-04 14:14:09 -0400

    Beecrowd
    Exercício 1068

---
+++ Beecrowd/bee1068.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string num;
+
+    while(getline(cin, num)){
+        int cont=0;
+        bool erro = false;
+        for(char c : num){
+            if(c == '('){
+                cont++;
+            }
+            else if( c == ')'){
+                cont--;
+                if(cont < 0){
+                    erro = true;
+                    break;
+                }
+            }
+        }
+
+        if(!erro && cont ==0){
+            cout << "correct\n";
+        }
+        else{
+            cout << "incorrect\n";
+        }
+    }   
+
+    return 0;
+}
